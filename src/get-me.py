583e75c6837ac864123a8dd1f0ea7fdeccf70c77
ieiriyuki <ieiriyuki@sur.bbiq.jp>
2021-02-23 10:00:05 +0900

    Add /me is added

---
+++ src/get-me.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+
+import os
+
+import requests
+
+# https://developers.facebook.com/docs/instagram-basic-display-api/guides/getting-profiles-and-media
+
+access_token = os.environ.get('ACCESS_TOKEN')
+base_url = 'https://graph.instagram.com/me'
+
+
+def main():
+    query = {'fields': 'id,username', 'access_token': access_token}
+    response = requests.get(base_url, params=query)
+    print(response.url)
+    print(response.json())
+
+
+if __name__ == '__main__':
+    main()
